4ed093a6e99cd86d2c82e4a9c4e2c28593dda332
Neo <mrrneo@outlook.com>
2025-09-02 19:47:33 +0600

    ...

---
+++ main.py
@@ -0,0 +1,64 @@
+import uvicorn
+import joblib
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+model = joblib.load('model_rf.pkl')
+scaler = joblib.load('scaler.pkl')
+
+col = ['tenure', 'MonthlyCharges', 'TotalCharges', 'Churn',
+       'Contract_One year',
+       'Contract_Two year',
+       'InternetService_Fiber optic',
+       'InternetService_No',
+       'OnlineSecurity_No internet service',
+       'OnlineSecurity_Yes',
+       'TechSupport_No internet service',
+       'TechSupport_Yes']
+
+class TelecomSchema(BaseModel):
+    tenure: int
+    MonthlyCharges: float
+    TotalCharges: float
+    Contract: str
+    InternetService: str
+    OnlineSecurity: str
+    TechSupport: str
+
+    class Config:
+        populate_by_name = True
+
+@app.post('/predict')
+async def predict(data:TelecomSchema):
+    data_dict = dict(data)
+
+    new_contract = data_dict.pop('Contract')
+    contract_binary = [
+        1 if new_contract == "One year" else 0,
+        1 if new_contract == "Two year" else 0]
+
+    new_internet_service = data_dict.pop('InternetService')
+    internet_binary = [
+        1 if new_internet_service == "Fiber optic" else 0,
+        1 if new_internet_service == "No" else 0]
+
+    new_online_security = data_dict.pop('OnlineSecurity')
+    security_binary = [
+        1 if new_online_security == "No internet service" else 0,
+        1 if new_online_security == "Yes" else 0]
+
+    new_tech_support = data_dict.pop('TechSupport')
+    support_binary = [
+        1 if new_tech_support == "No internet service" else 0,
+        1 if new_tech_support == "Yes" else 0]
+
+    features = list(data_dict.values()) + contract_binary + internet_binary + security_binary + support_binary
+    scaled = scaler.transform([features])
+    prediction = model.predict(scaled)[0]
+    return {'Churn answer': 'Yes' if prediction == 1 else 'No'}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
